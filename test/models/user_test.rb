5cd83745bd0adade477017f4eed74cd8cbb5a59c
GED <gimaev.ufa@gmail.com>
2023-08-16 21:32:21 +0500

    feat: instance method for user test(level)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def tests_by_level(level)
+    tests.where(level: level)
+  end
 end
