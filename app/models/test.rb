616b84df785af58bcc0915d3ea9e4bf1050addad
GED <gimaev.ufa@gmail.com>
2023-08-21 11:50:17 +0500

    feat: added self.category_tests_titles_desc to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,15 +1,10 @@
 class Test < ApplicationRecord
 
-  scope :by_category, ->(category) { joins(:category).where(category: { title: category }) }
-
   def self.category_tests_titles_desc(category)
-    Test.joins ('JOIN categories ON tests category_id = categories. id')
-    Test.order(title: :desc).pluck(:title)
+
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order(title: :desc).pluck(:title)
+
   end
 
-    def self.test_by_category_desc(category)
-      Test.order(title: :desc).pluck(:title)
-    end
 end
 
-:by_category_title, -> (title) {  joins(:category).where('categories.title = ?', title) }
