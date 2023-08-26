73fd5991125d083d1d3ebd7de7f199857d9defe0
GED <gimaev.ufa@gmail.com>
2023-08-26 19:26:48 +0500

    feat: added def tests_by_level to app/models/User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 
-  has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
+  #has_many :tests_users
+  #has_many :tests, through: :tests_users
   #has_and_belongs_to_many :tests
 
   def tests_by_level(level)
-    Test.where('tests.level = ?', level)
+    PassingTest.joins('JOIN users ON passing_tests.user_id = users.id').joins('JOIN tests ON passing_tests.test_id = tests.id').where('tests.level = ?', level).pluck(:user, :title, :level)
   end
 end
