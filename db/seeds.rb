f8eff12105fdc177c7c29af292366854d1e68298
GED <gimaev.ufa@gmail.com>
2023-08-26 15:08:19 +0500

    fix: added PassingTest.create! to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,5 +41,14 @@ answers = Answer.create!(
   ]
 )
 
+passing_tests = PassingTest.create!(
+  [
+    { user_id: users[0].id, test_id: tests[0].id },
+    { user_id: users[1].id, test_id: tests[1].id },
+    { user_id: users[2].id, test_id: tests[2].id },
+    { user_id: users[2].id, test_id: tests[1].id },
+    { user_id: users[2].id, test_id: tests[0].id }
 
+  ]   
+)
   
