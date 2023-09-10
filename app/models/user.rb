77f2b54dcf90c416d037adafecead8712223c8ed
GED <gimaev.ufa@gmail.com>
2023-09-10 16:47:06 +0500

    fix: tests_by_level changed to TestsUser, requiest via Test still does not work - (Object doesn't support #inspect)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-
-  #has_and_belongs_to_many :tests
+  
   has_many :tests_users
   has_many :tests, through: :tests_users
 
   def tests_by_level(level)
-    PassingTest.joins('JOIN tests ON tests.id = passing_tests.test_id').where(user_id: self.id).where('tests.level = ?', level).pluck(:user_id, :title )
+    #Test.joins('JOIN tests_users ON tests.id = tests_users.test_id').where(user_id: self).where('tests.level = ?', level) => (Object doesn't support #inspect) I've not figured it out yet
+    TestsUser.joins('JOIN tests ON tests.id = tests_users.test_id').joins('JOIN users ON users.id = tests_users.user_id').where(user_id: self).where('tests.level = ?', level).pluck(:user, :title)
   end
 end
