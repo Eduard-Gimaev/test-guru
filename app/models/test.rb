c63b76a87f6b41f1e29ad28f1f9b83e7c729f611
GED <gimaev.ufa@gmail.com>
2023-09-11 10:09:10 +0500

    fix: a query via TEST for user works now

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
-  #has_and_belongs_to_many :users
   has_many :tests_users
   has_many :users, through: :tests_users
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
 
   def tests_by_level(level)
-    #Test.joins('JOIN tests_users ON tests.id = tests_users.test_id').where(user_id: self).where('tests.level = ?', level) => (Object doesn't support #inspect) I've not figured it out yet
-    TestsUser.joins('JOIN tests ON tests.id = tests_users.test_id').joins('JOIN users ON users.id = tests_users.user_id').where(user_id: self).where('tests.level = ?', level).pluck(:user, :title)
+    Test.joins('JOIN tests_users ON tests.id = tests_users.test_id').joins('JOIN users ON users.id = tests_users.user_id').where('user_id = ?', self.id).where('tests.level = ?', level).pluck(:user, :title)
   end
 end
