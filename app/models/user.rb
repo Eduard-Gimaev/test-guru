9bc7e082c54610e59ac8ed83adecc28fbf074bf4
GED <gimaev.ufa@gmail.com>
2023-09-12 15:01:25 +0500

    fix: removed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  #has_many :tests_users
+  #has_many :tests, through: :tests_users
 
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests.id = tests_users.test_id').joins('JOIN users ON users.id = tests_users.user_id').where('user_id = ?', self.id).where('tests.level = ?', level).pluck(:user, :title)
