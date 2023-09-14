eee6c7e411fe4740ad85995bf22ab04188ac0a0e
GED <gimaev.ufa@gmail.com>
2023-09-14 18:10:22 +0500

    fix: refactored test_by_level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,13 +5,9 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  
-
-  
   def self.category_tests_titles_desc(category)
-    #Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order(title: :desc)
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order(title: :desc)
     
   end
-
 end
 
