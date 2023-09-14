53669ec03841ef3290ed81166e32e301aa96a63d
GED <gimaev.ufa@gmail.com>
2023-09-14 09:36:12 +0500

    feat:added user_id to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  #belongs_to :user
+
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
+
+  
+
   
   def self.category_tests_titles_desc(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order(title: :desc)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_10_102214) do
+ActiveRecord::Schema.define(version: 2023_09_13_092725) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2023_09_10_102214) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 2023_09_10_102214) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
