8710853f6c95679952989a844b393b92ad5c3c1a
GED <gimaev.ufa@gmail.com>
2023-09-16 11:33:54 +0500

    feat: added email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :authored_tests, foreign_key: "author_id", class_name: "Test"
 
-
-  
-
   def tests_by_level(level)
     tests.where(level: level).pluck(:title, :level)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_13_092725) do
+ActiveRecord::Schema.define(version: 2023_09_16_063136) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2023_09_13_092725) do
     t.string "user", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
   end
 
   add_foreign_key "answers", "questions"
