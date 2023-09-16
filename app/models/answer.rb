15de0edf6197e2c9453cf0c65e241c467c21f621
GED <gimaev.ufa@gmail.com>
2023-09-16 12:42:47 +0500

    feat: added validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,11 @@
 class Answer < ApplicationRecord
     belongs_to :question
     scope :correct, -> { where(correct: true)}
+
+    validates :body, presence: true
+    validates :validate_answers_count
+
+    def validate_answers_count
+        errors.add(:base, "One Question can have from 1 to 4 answers.") if question.answers.count >= 4
+    end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,12 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  scope :by_category, -> (category_title) {joins(:category).where('categories.title = ?', category_title)}
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true }
+  validates :validate_level_max
+  #validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  scope :by_category, -> (category_title) {joins(:category).where('categories.title = ?', category_title)}
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :difficult, -> { where(level: 5..Float::INFINITY) }
@@ -14,6 +18,10 @@ class Test < ApplicationRecord
 
   def self.category_tests_titles_desc(category)
     by_category(category).order(title: :desc).pluck(:title)
+  end
+
+  def validate_level_max
+    errors.add(:level) if level.to_i > 0
   end
 end
 
