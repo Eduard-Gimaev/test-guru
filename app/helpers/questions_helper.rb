ed957108c3cd5d39731e1736c41526f474851110
GED <gimaev.ufa@gmail.com>
2023-10-02 14:42:28 +0500

    feat: added helpers for creating/editing questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.new_record?
+      "Create a new question for the test: #{question.test.title}"
+    else
+      "Edit a question for the test: #{question.test.title}"
+    end
+  end
 end
