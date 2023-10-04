49eaa2437992cb6f4af3401485a24635afe1f354
GED <gimaev.ufa@gmail.com>
2023-10-04 12:36:31 +0500

    fix: changed link to the github without def in the application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,6 @@ module ApplicationHelper
   def current_year
     " - #{Time.now.year.to_i} - "
   end
-
-  def github_url(author, repo)
-    url = "https://github.com/#{author}/#{repo}"
-    # link_to repo.capitalize, url, target: "_blank", rel: "nofollow noopener", class: "nav-link px-2"
-  end
 end
 
 
