b9d7d012a20eabdb2425652cdb8f80a4d3d35aed
GED <gimaev.ufa@gmail.com>
2023-10-14 21:30:33 +0500

    fix:refactoring margins

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,7 +4,6 @@ class TestsController < ApplicationController
   before_action :find_test, only: %i[start show edit update destroy]
   before_action :find_user, only: :start
 
-
   def index
   end
 
