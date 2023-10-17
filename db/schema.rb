6e4bd1ece1335f9263ae7eb913f115c6571d75ea
GED <gimaev.ufa@gmail.com>
2023-10-17 15:52:33 +0500

    fix: changed answers correct from string to boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_08_140832) do
+ActiveRecord::Schema.define(version: 2023_10_17_100140) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.string "correct", default: "f"
+    t.boolean "correct", default: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
