d62e9dec3dcfe17c6e63c77edd021fd513b9bbba
GED <gimaev.ufa@gmail.com>
2023-10-22 14:06:00 +0500

    feat: adde sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-  
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to tests_path
@@ -12,6 +11,11 @@ class SessionsController < ApplicationController
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path
   end
   
 end
