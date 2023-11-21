eef8c93a0ea9f4a0675c1171da50e9cd6fe7fc8c
GED <gimaev.ufa@gmail.com>
2023-11-21 19:40:49 +0500

    feat: added gist_question create on the githubGist

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,8 +1,18 @@
 class GitHubClient
-    ROOT_ENDPOINT = 'http://api.github.com'
+    ROOT_ENDPOINT = 'https://api.github.com'
+    ACCESS_TOKEN = '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'
+
 
     def initialize
       @http_client = setup_http_client
+    end
+
+    def create_gist(params)
+        @http_client.post('gists') do |request|
+          request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+          request.headers['Content-type'] = 'application/json'
+          request.body = params.to_json
+        end
     end
 
     private
