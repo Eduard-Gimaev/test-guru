189a69e3fa75f117cf98937e1b5998c4745efc8c
GED <gimaev.ufa@gmail.com>
2023-12-25 15:58:32 +0500

    feat: added function sortedRows

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,7 +1,21 @@
 document.addEventListener('turbolinks:load', function(){
   var control = document.querySelector('.sort-by-title');
 
-  control.addEventListener('click', function(){
-    alert('Click');
-  });
+  control.addEventListener('click', sortRowsByTitle);
 }); 
+
+function sortRowsByTitle(){
+  //Nodelist
+  //https://developer.mozilla.org/ru/docs/Web/API/NodeList
+
+  var table = document.querySelector('table');
+  var rows = table.querySelectorAll('tr');
+  var sortedRows = [];
+
+  //select all rows except the very first one which is the header
+
+  for( var i = 1; i < rows.length; i++ ){
+    sortedRows.push(rows[i]);
+  }
+  console.log(sortedRows)
+}
