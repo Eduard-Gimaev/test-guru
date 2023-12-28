04bf41f3032d7d1b036071db2ecfb14e256d6706
GED <gimaev.ufa@gmail.com>
2023-12-28 19:40:51 +0500

    fix: added function for check_result and hide_notice while input is empty

--- app/javascript/utilities/confirmation.js
+++ app/javascript/utilities/confirmation.js
@@ -3,17 +3,31 @@ document.addEventListener('turbolinks:load', function(){
     var passwordConfirmation = document.getElementById('user_password_confirmation')
 
     if (password && passwordConfirmation) {passwordConfirmation.addEventListener('input', password_check)}
-  })
+})
 
 
 function password_check() {
-    if (document.getElementById('user_password').value ==
-        document.getElementById('user_password_confirmation').value) {
-            document.querySelector('.octicon-alert').classList.add('hide')
-            document.querySelector('.octicon-check-circle-fill').classList.remove('hide')
+    if ( document.getElementById('user_password_confirmation').value !== "") {
+        if (document.getElementById('user_password').value ==
+            document.getElementById('user_password_confirmation').value) {
+            check_fail()
+        } else {
+            check_success()
+        }
     } else {
-        document.querySelector('.octicon-alert').classList.remove('hide')
-        document.querySelector('.octicon-check-circle-fill').classList.add('hide')
+        hide_notices()
     }
 }
-    
+
+function check_success() {
+    document.querySelector('.octicon-alert').classList.remove('hide')
+    document.querySelector('.octicon-check-circle-fill').classList.add('hide')
+}
+function check_fail(){
+    document.querySelector('.octicon-alert').classList.add('hide')
+    document.querySelector('.octicon-check-circle-fill').classList.remove('hide')
+}
+function hide_notices(){
+    document.querySelector('.octicon-alert').classList.add('hide')
+    document.querySelector('.octicon-check-circle-fill').classList.add('hide')
+}
