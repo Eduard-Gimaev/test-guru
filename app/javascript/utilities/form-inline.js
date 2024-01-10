16ba772c6cb39b0ecdb71d9f603be2e48aeff171
GED <gimaev.ufa@gmail.com>
2024-01-10 11:27:42 +0500

    feat: added toggle for changing test.title

--- app/javascript/utilities/form-inline.js
+++ app/javascript/utilities/form-inline.js
@@ -1,20 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-    var controls = document.querySelectorAll('.form-inline-link')
-
-    if (controls.length){
-      for (var i = 0;  i < controls.length; i++){
-        controls[i].addEventListener('click', formInlineLinkHandler)
-      }
-    }
-
-    var errors = document.querySelector('.resource-errors')
-
-    if (errors) {
-      var resourceId = errors.dataset.resourceId
-      formInlineHandler(resourceId)
+    
+    if ($('.form-inline-link').length) {
+        $('.form-inline-link').on('click', formInlineLinkHandler)
+        var errors = document.querySelector('.resource-errors')
+        if (errors) {
+            var resourceId = errors.dataset.resourceId
+            formInlineHandler(resourceId)
+        }
     }
 })
-
 
 function formInlineLinkHandler(event){
     event.preventDefault()
@@ -23,17 +17,16 @@ function formInlineLinkHandler(event){
 }
 
 function formInlineHandler(testId){
-    var link = document.querySelector('.form-inline-link[data-test-id = "' + testId + '"]')
-    var testTitle = document.querySelector('.test-title[data-test-id = "' + testId + '"] ')
-    var formInline = document.querySelector('.form-inline[data-test-id = "' + testId + '"]')
+    var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
+    var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+    var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
 
-    if (formInline.classList.contains('hide')) {
-        testTitle.classList.add('hide')
-        formInline.classList.remove('hide')
+    $testTitle.toggle()
+    $formInline.toggle()
+
+    if ($formInline.is(':visible')) {
         link.value = 'Cancel'
     }else{
-        testTitle.classList.remove('hide')
-        formInline.classList.add('hide')
         link.value = 'Edit'
     }
     
