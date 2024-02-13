c4fa89773ecf13c3cb2309ef5f46cf2359df0929
GED <gimaev.ufa@gmail.com>
2024-02-13 16:34:59 +0500

    feat: added timer to test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_10_072249) do
+ActiveRecord::Schema.define(version: 2024_02_13_113244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2024_02_10_072249) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
     t.boolean "publish", default: false
+    t.integer "timer"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
