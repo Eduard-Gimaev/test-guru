e2bb327693a0612e16536d0df8f4fbf2f6e69b99
GED <gimaev.ufa@gmail.com>
2024-03-05 14:14:41 +0500

    feat: backend_part for timer wors well, but application.js icould not import timer.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+alert(1)
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -17,3 +19,4 @@ import "utilities/timer"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
--- app/javascript/utilities/progress-bar.js
+++ app/javascript/utilities/progress-bar.js
@@ -1,3 +1,15 @@
 
+document.addEventListener('turbolinks:load', function() {
+    var progressBar = document.querySelector('.progress-bar')
+    setProgress(progressBar)
+    
+})
 
-console.log("custom js file loaded")
+function setProgress(progressBar){
+    var currentQuestion = progressBar.dataset.currentQuestion - 1
+    var allQuestions = progressBar.dataset.allQuestions
+    var progress = Math.round(currentQuestion / allQuestions * 100)
+
+    progressBar.setAttribute('style', 'width:' + progress + '%')
+    progressBar.innerText = Math.round(progress) + '%'
+}
