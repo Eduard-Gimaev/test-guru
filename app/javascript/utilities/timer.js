7865470d380b7c5d6bd8e3f55e7c2a173bffe4e0
GED <gimaev.ufa@gmail.com>
2024-03-05 11:31:26 +0500

    fix: del package-lock.json

--- app/javascript/utilities/timer.js
+++ app/javascript/utilities/timer.js
@@ -1,19 +1,21 @@
-document.addEventListener('turbolinks:load', function() {
-    var timer = document.querySelector('.timer')
-    if (timer) { countdown(timer) }
-});
+// document.addEventListener('turbolinks:load', function() {
+//     var timer = document.querySelector('.timer')
+//     if (timer) { countdown(timer) }
+// });
 
-function countdown(timer){
-  var timeStart = Date.now()
-  var timeEnd = timeStart + (timer.dataset.timer * 1000)
-  var timeLeft = (timeEnd - timeStart)/1000
+// function countdown(timer){
+//   var timeStart = Date.now()
+//   var timeEnd = timeStart + (timer.dataset.timer * 1000)
+//   var timeLeft = (timeEnd - timeStart)/1000
 
-  setInterval(function(){
-    document.querySelector('.timer').textContent = 'timeLeft: ' + timeLeft 
-    timeLeft -= 1
+//   setInterval(function(){
+//     document.querySelector('.timer').textContent = 'timeLeft: ' + timeLeft 
+//     timeLeft -= 1
 
-    if (timeLeft < 0 ) {
-        document.querySelector('.timer').textContentn = ' Time is up, please press the NEXT'
-    }
-  })
-}
+//     if (timeLeft < 0 ) {
+//         document.querySelector('.timer').textContentn = ' Time is up, please press the NEXT'
+//     }
+//   })
+// }
+
+console.log("custom js file loaded")
