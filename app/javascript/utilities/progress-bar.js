cdb1c16504d058301692201f2507c73d911230c1
GED <gimaev.ufa@gmail.com>
2024-03-05 12:55:11 +0500

    feat: fix point for resolving problem with import js failes

--- app/javascript/utilities/progress-bar.js
+++ app/javascript/utilities/progress-bar.js
@@ -1,15 +1,3 @@
 
-document.addEventListener('turbolinks:load', function() {
-    var progressBar = document.querySelector('.progress-bar')
-    setProgress(progressBar)
-    
-})
 
-function setProgress(progressBar){
-    var currentQuestion = progressBar.dataset.currentQuestion - 1
-    var allQuestions = progressBar.dataset.allQuestions
-    var progress = Math.round(currentQuestion / allQuestions * 100)
-
-    progressBar.setAttribute('style', 'width:' + progress + '%')
-    progressBar.innerText = Math.round(progress) + '%'
-}
+console.log("custom js file loaded")
--- app/javascript/utilities/timer.js
+++ app/javascript/utilities/timer.js
@@ -1,21 +1,2 @@
-// document.addEventListener('turbolinks:load', function() {
-//     var timer = document.querySelector('.timer')
-//     if (timer) { countdown(timer) }
-// });
-
-// function countdown(timer){
-//   var timeStart = Date.now()
-//   var timeEnd = timeStart + (timer.dataset.timer * 1000)
-//   var timeLeft = (timeEnd - timeStart)/1000
-
-//   setInterval(function(){
-//     document.querySelector('.timer').textContent = 'timeLeft: ' + timeLeft 
-//     timeLeft -= 1
-
-//     if (timeLeft < 0 ) {
-//         document.querySelector('.timer').textContentn = ' Time is up, please press the NEXT'
-//     }
-//   })
-// }
 
 console.log("custom js file loaded")
